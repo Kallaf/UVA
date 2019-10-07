a74dea7e7de1d0c4006febdf687cecd2c925af9c
Ahmed Elkalaf <kalaf1997@yahoo.com>
2019-10-08 01:17:50 +0200

    Create UVa 11597.cpp

---
+++ MATHEMATICS/Combinatorics/Others, Easier/UVa 11597.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	//--------------------------
+	for(int i=1;cin >> n,n!=0;i++)cout << "Case "<< i << ": " << (n/2) << endl;
+    return 0;
+}
